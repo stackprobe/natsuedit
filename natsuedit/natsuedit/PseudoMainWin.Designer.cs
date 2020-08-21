147e20158fe9cedbe91cd18d91465e4e0f058b2b
stackprobe <stackprobes@gmail.com>
2020-08-21 10:03:05 +0900

    Juliet

--- natsuedit/natsuedit/PseudoMainWin.Designer.cs
+++ natsuedit/natsuedit/PseudoMainWin.Designer.cs
@@ -42,7 +42,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.ClientSize = new System.Drawing.Size(284, 261);
 			this.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Location = new System.Drawing.Point(-400, -400);
